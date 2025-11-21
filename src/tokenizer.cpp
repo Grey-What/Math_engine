d9623fcb26513d497652a066b6f0c80f81739998
Grey <greywhat2002@gmail.com>
2025-11-21 18:12:29 +0200

    Implemented method to tokenize functions and variables

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -1,5 +1,6 @@
 // Tokenizer Implementation
 #include "C:\Users\greyw\VSC_CodeCreations\Math_engine\include\tokenizer.h"
+#include "C:\Users\greyw\VSC_CodeCreations\Math_engine\include\token.h"
 #include <cctype>
 #include <string>
 #include <vector>
@@ -21,11 +22,39 @@ void Tokenizer::skipWhiteSpace() {
   while (isspace(peek())) get();
 }
 
+TokenType Tokenizer::readNumber(string& buffer) {
+  buffer.clear();
+  buffer.push_back(get());
+  while (isdigit(peek()) || peek() == '.')
+  {
+    buffer.push_back(get());
+  }
+  return TokenType::NUMBER;
+}
+
+TokenType Tokenizer::readLetter(string& buffer) {
+  bool isFunction = false;
+  buffer.clear();
+  while (isalpha(peek()))
+  {
+    buffer.push_back(get());
+  }
+  //check if function
+  for (int i = 0; i < FUNCTION_TOKEN_SIZE; i++)
+  {
+    if (buffer == FUNCTION_TOKENS[i])
+    {
+      return TokenType::FUNCTION;
+    }
+  }
+  return TokenType::VARIABLE;
+}
+
 vector<Token> Tokenizer::tokenize() {
   vector<Token> tokens;
   char currentChar;
   string buffer = "";
-  int type = 11;
+  TokenType type = TokenType::UNKNOWN;
 
   while (pos < input.size())
   {
@@ -33,13 +62,13 @@ vector<Token> Tokenizer::tokenize() {
     currentChar = peek();
     if (isdigit(peek()) || peek() == '.')
     {
-      readNumber(buffer);
-      tokens.push_back(Token(TokenType::NUMBER, buffer));
+      type = readNumber(buffer);
+      tokens.push_back(Token(type, buffer));
     }
     if (isalpha(peek()))
     {
-      type = readLetters(buffer);
-      tokens.push_back(Token(static_cast<TokenType>(type), buffer));
+      type = readLetter(buffer);
+      tokens.push_back(Token(type, buffer));
 
     }
     if (SINGLE_CHAR_TOKENS.find(peek()) != string::npos)

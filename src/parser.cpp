ac2fe564e1ffe953e01b4a577f1596571524a54e
Grey <greywhat2002@gmail.com>
2025-11-27 19:36:54 +0200

    implemented next ADT constuctor members, completed recursive decent

--- src/parser.cpp
+++ src/parser.cpp
@@ -6,15 +6,76 @@
 Parser::Parser(const vector<Token>& t) : tokens(t) ,pos(0) {}
 
 //View members
-Token Parser::peek() {}
-Token Parser::get() {}
+Token Parser::peek() {
+
+}
+
+Token Parser::get() {
+
+}
 
 //Parsing member functions
-ASTNode* Parser::parseExpression() {}
-ASTNode* Parser::parseTerm() {}
-ASTNode* Parser::parseFactor() {}
-ASTNode* Parser::parsePrimary() {}
+ASTNode* Parser::parseExpression() {
+  ASTNode* left = parseTerm();
+  while (peek().type == TokenType::OP_PLUS || peek().type == TokenType::OP_MINUS)
+  {
+    Token opToken = get();
+    ASTNode* right = parseTerm();
+    ASTNode* opNode = new ASTNode(opToken);
+    opNode->right = right;
+    opNode->left = left;
+    left = opNode;
+  }
+  return left;
+}
+
+ASTNode* Parser::parseTerm() {
+  ASTNode* left = parseFactor();
+  while (peek().type == TokenType::OP_MUL || peek().type == TokenType::OP_DIV)
+  {
+    Token OpToken = get();
+    ASTNode* right = parseFactor();
+    ASTNode* opNode = new ASTNode(OpToken);
+    opNode->right = right;
+    opNode->left = left;
+    left = opNode;
+  }
+  return left;
+}
+
+ASTNode* Parser::parseFactor() {
+  if (peek().type == TokenType::OP_PLUS || peek().type == TokenType::OP_MINUS)
+  {
+    Token opToken = get();
+    ASTNode* operand = parseFactor();
+    ASTNode* opNode = new ASTNode(opToken);
+    opNode->left = operand;
+    return opNode;
+  }
+  if (peek().type == TokenType::NUMBER)
+  {
+    Token token = get();
+    return new ASTNode(token);
+  }
+  if (peek().type == TokenType::VARIABLE)
+  {
+    Token token = get();
+    return new ASTNode(token);
+  }
+  if (peek().type == TokenType::FUNCTION)
+  {
+    Token token = get();
+    ASTNode* arg = parseFactor();
+    ASTNode* funcNode = new ASTNode(token);
+    funcNode->left = arg;
+    return funcNode;
+  }
+}
+
+ASTNode* Parser::parsePrimary() {
+
+}
 
 //Entry
-ASTNode* parse() {};
-
+ASTNode* parse() {
+}

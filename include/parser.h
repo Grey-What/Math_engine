385339bd2238e263b3fa5fdaccc0bfaff2936bc9
Grey <greywhat2002@gmail.com>
2025-11-27 18:19:41 +0200

    Parser interface file

--- include/parser.h
+++ include/parser.h
@@ -1,2 +1,31 @@
 // Parser class declaration
 // Interface file
+// parser.h
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "token.h"
+#include "ASTNode.h"
+#include <vector>
+
+class Parser {
+private:
+    std::vector<Token> tokens;
+    size_t pos;
+
+    Token peek();
+    Token get();
+
+    // Parsing functions
+    ASTNode* parseExpression(); // Handles + and -
+    ASTNode* parseTerm();       // Handles * and /
+    ASTNode* parseFactor();     // Handles ^ and unary operators
+    ASTNode* parsePrimary();    // Handles numbers, variables, functions, parentheses
+
+public:
+    Parser(const std::vector<Token>& t);
+    ASTNode* parse();                     // Entry point, returns root of AST
+};
+
+#endif
+
